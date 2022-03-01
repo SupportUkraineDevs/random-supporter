161dbb05f2ea455df18b89f7105654206d7c22fe
Matteo Fordiani <matteofordiani@gmail.com>
2022-03-01 22:25:34 +0100

    switched to self-hosted gsx2json api

--- main.js
+++ main.js
@@ -5,6 +5,10 @@ const version = "1.0.2";
 /*
 * Init cycle to get cookie with already visited devs, check if api version has changed and in case delete cookies
 */
+const googleSx2JsonApi = "https://gsx2json.com/";
+const customGoogleSx2JsonApi = "https://supportukrainedevs-mygsx2json.herokuapp.com/";
+const googleSpreadsheetApiParameters = "api?id=1mmG25XY0vd8DIGvg6pASx3E-MoOGscU2yCMealPq4RM&sheet=Sheet1";
+let apiUrl = customGoogleSx2JsonApi;
 let alreadySupported;
 (function () {
 
@@ -18,7 +22,18 @@ let alreadySupported;
   setCookie('api-version', version, 7);
   
   alreadySupported = getCookie('already-supported') ? new Set(getCookie('already-supported').split(',')) : new Set();
-  
+
+  fetch(googleSx2JsonApi).then(r => {
+    switch(r.status) {
+        case 200:
+            apiUrl = googleSx2JsonApi;
+            break;
+        default:
+            apiUrl = customGoogleSx2JsonApi;
+            break;
+    }
+  });
+
 })();
 
 /*
@@ -28,25 +43,26 @@ function supportRandomDev(){
   /*
   * Put whole function in 'big-button' onclick handler to avoid popup blockers
   */
-  var url = "https://gsx2json.com/api?id=1mmG25XY0vd8DIGvg6pASx3E-MoOGscU2yCMealPq4RM&sheet=Sheet1";         
+      
+    let url =  apiUrl + googleSpreadsheetApiParameters;   
     fetch(url).then(data => data.json())        
     .then(function( data ) {           
-      if(alreadySupported.length >= data.rows.length) {            
-        alreadySupported = new Set();          
-      }           
-      let dev = getRandomElement(data.rows);           
-      let found = false;          
-      do {            
-        if (dev.profile && !alreadySupported.has(dev.profile)) {               
-          alreadySupported.add(dev.profile);               
-          setCookie("already-supported", [...alreadySupported].join(), 7);               
-          openInNewTab(dev.profile);               
-          found = true; 
-          return;             
-          } else {               
-            dev = getRandomElement(data.rows);     
-          }           
-      } while (found === false);                    
+        if(alreadySupported.length >= data.rows.length) {            
+            alreadySupported = new Set();          
+        }           
+        let dev = getRandomElement(data.rows);           
+        let found = false;          
+        do {            
+            if (dev.profile && !alreadySupported.has(dev.profile)) {               
+                alreadySupported.add(dev.profile);               
+                setCookie("already-supported", [...alreadySupported].join(), 7);               
+                openInNewTab(dev.profile);               
+                found = true; 
+                return;             
+            } else {               
+                dev = getRandomElement(data.rows);     
+            }           
+        } while (found === false);                    
     });
 }
 function getRandomInt(max) { return Math.floor(Math.random() * max); }
